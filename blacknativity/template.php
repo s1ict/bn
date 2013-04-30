abbc9252312481244b8a332719d77d68b2d8ef7a
Sekou Perry <sekou.perry@gmail.com>
2013-04-30 16:26:48 -0400

    added trash folder for git and function for search

--- blacknativity/template.php
+++ blacknativity/template.php
@@ -12,10 +12,10 @@ drupal_add_js(drupal_get_path('theme', 'blacknativity') . '/js/bluemasters.js');
 <?php
 function blacknativity_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
-    $form['search_block_form']['#title'] = t('Search Keywords'); // Change the text on the label element
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
     $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
     $form['search_block_form']['#size'] = 40;  // define size of the textfield
-    $form['search_block_form']['#default_value'] = t('Search'); // Set a default value for the textfield
+    //$form['search_block_form']['#default_value'] = t('Search Keywords'); // Set a default value for the textfield
   
 
     // Add extra attributes to the text box
@@ -25,7 +25,7 @@ function blacknativity_form_alter(&$form, &$form_state, $form_id) {
     $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
 
     // Alternative (HTML5) placeholder attribute instead of using the javascript
-    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search Keywords');
   }
 }
 ?>
